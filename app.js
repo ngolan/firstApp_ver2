0c9aad1087fb11b0cd12882a2331351874dc2309
ngolan <golann@ui.co.il>
2015-02-11 17:48:39 +0200

    cg-angular yomen generator install

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('latet', ['ui.bootstrap','ui.router']);
+
+angular.module('latet').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('latet').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
